4338553c04a92ecff4d62119d4e5bfd4a7e61935
ArtemOstapchuk01 <81463913+ArtemOstapchuk01@users.noreply.github.com>
2024-01-06 22:55:22 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Calculator.sum(13, 8));
-        System.out.println(Calculator.sum(3,9));
     }
 }
